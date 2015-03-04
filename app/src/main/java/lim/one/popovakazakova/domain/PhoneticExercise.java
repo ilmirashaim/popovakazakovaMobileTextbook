eee22e4657a4b92d552a8c91667755617f7e24a5
mira <ilmirashaim@gmail.com>
2015-03-04 12:40:48 +0500

    sound usage domain, mp3 files for phonetic ex

--- app/src/main/java/lim/one/popovakazakova/domain/PhoneticExercise.java
+++ app/src/main/java/lim/one/popovakazakova/domain/PhoneticExercise.java
@@ -1,8 +1,35 @@
 package lim.one.popovakazakova.domain;
 
 public class PhoneticExercise {
+    private Long id;
     private String filename;
-    Long soundId;
+    private Long soundId;
+    private String transcription;
+    private String title;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTranscription() {
+        return transcription;
+    }
+
+    public void setTranscription(String transcription) {
+        this.transcription = transcription;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
     public String getFilename() {
         return filename;
---
+++ app/src/main/java/lim/one/popovakazakova/domain/SoundUsage.java
@@ -0,0 +1,49 @@
+package lim.one.popovakazakova.domain;
+
+public class SoundUsage {
+    private Long id;
+    private Long soundId;
+    private String examples;
+    private String position;
+    private String spelling;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getSoundId() {
+        return soundId;
+    }
+
+    public void setSoundId(Long soundId) {
+        this.soundId = soundId;
+    }
+
+    public String getExamples() {
+        return examples;
+    }
+
+    public void setExamples(String examples) {
+        this.examples = examples;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getSpelling() {
+        return spelling;
+    }
+
+    public void setSpelling(String spelling) {
+        this.spelling = spelling;
+    }
+}
