da7a73282a1331adc3d0007d9a4c97948fc6e838
mira <ilmirashaim@gmail.com>
2015-03-26 21:05:38 +0500

    lollipop 5.1, float button for old versions

--- app/src/main/java/lim/one/popovakazakova/util/view/FloatingActionButton.java
+++ app/src/main/java/lim/one/popovakazakova/util/view/FloatingActionButton.java
@@ -16,13 +16,28 @@
 
 package lim.one.popovakazakova.util.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
 import android.graphics.Outline;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.LayerDrawable;
+import android.graphics.drawable.RippleDrawable;
+import android.graphics.drawable.StateListDrawable;
+import android.os.Build;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
 import android.widget.Checkable;
 import android.widget.FrameLayout;
+
+import lim.one.popovakazakova.R;
 
 /**
  * A Floating Action Button is a {@link android.widget.Checkable} view distinguished by a circled
@@ -56,6 +71,19 @@ public class FloatingActionButton extends FrameLayout implements Checkable {
 
     // A boolean that tells if the FAB is checked or not.
     private boolean mChecked;
+    public static final int TYPE_NORMAL = 0;
+    public static final int TYPE_MINI = 1;
+
+    private boolean mVisible;
+
+    private int mColorNormal;
+    private int mColorPressed;
+    private int mColorDisabled;
+    private boolean mShadow = true;
+    private int mColorRipple;
+    private int mType;
+    private int mShadowSize;
+    private boolean mMarginsSet;
 
     // A listener to communicate that the FAB has changed it's state
     private OnCheckedChangeListener mOnCheckedChangeListener;
@@ -66,44 +94,68 @@ public class FloatingActionButton extends FrameLayout implements Checkable {
 
     public FloatingActionButton(Context context, AttributeSet attrs) {
         this(context, attrs, 0, 0);
+        init(context, attrs);
     }
 
     public FloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr) {
         this(context, attrs, defStyleAttr, 0);
+        init(context, attrs);
     }
 
     public FloatingActionButton(Context context, AttributeSet attrs, int defStyleAttr,
-            int defStyleRes) {
+                                int defStyleRes) {
         super(context, attrs, defStyleAttr);
 
         setClickable(true);
+        init(context, attrs);
+    }
 
-        // Set the outline provider for this view. The provider is given the outline which it can
-        // then modify as needed. In this case we set the outline to be an oval fitting the height
-        // and width.
-        setOutlineProvider(new ViewOutlineProvider() {
-            @Override
-            public void getOutline(View view, Outline outline) {
-                outline.setOval(0, 0, getWidth(), getHeight());
+    private void init(Context context, AttributeSet attributeSet) {
+        mVisible = true;
+        mColorNormal = getColor(R.color.fab_color_1);
+        mColorPressed = getColor(R.color.fab_color_2);
+        mColorRipple = getColor(android.R.color.white);
+        mColorDisabled = getColor(android.R.color.darker_gray);
+        mType = TYPE_NORMAL;
+        mShadow = true;
+        mShadowSize = getDimension(R.dimen.fab_shadow_size);
+        if (attributeSet != null) {
+            initAttributes(context, attributeSet);
+        }
+        updateBackground();
+    }
+
+    private void initAttributes(Context context, AttributeSet attributeSet) {
+        TypedArray attr = getTypedArray(context, attributeSet, R.styleable.FloatingActionButton);
+        if (attr != null) {
+            try {
+                mColorNormal = attr.getColor(R.styleable.FloatingActionButton_fab_colorNormal,
+                        getColor(R.color.fab_color_1));
+                mColorPressed = attr.getColor(R.styleable.FloatingActionButton_fab_colorPressed,
+                        getColor(R.color.fab_color_2));
+                mColorRipple = attr.getColor(R.styleable.FloatingActionButton_fab_colorRipple,
+                        getColor(android.R.color.white));
+                mColorDisabled = attr.getColor(R.styleable.FloatingActionButton_fab_colorDisabled,
+                        mColorDisabled);
+                mShadow = attr.getBoolean(R.styleable.FloatingActionButton_fab_shadow, true);
+                mType = attr.getInt(R.styleable.FloatingActionButton_fab_type, TYPE_NORMAL);
+            } finally {
+                attr.recycle();
             }
-        });
-
-        // Finally, enable clipping to the outline, using the provider we set above
-        setClipToOutline(true);
+        }
     }
 
     /**
      * Sets the checked/unchecked state of the FAB.
+     *
      * @param checked
      */
     public void setChecked(boolean checked) {
-        // If trying to set the current state, ignore.
         if (checked == mChecked) {
             return;
         }
         mChecked = checked;
 
-        // Now refresh the drawable state (so the icon changes)
         refreshDrawableState();
 
         if (mOnCheckedChangeListener != null) {
@@ -143,10 +195,8 @@ public class FloatingActionButton extends FrameLayout implements Checkable {
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        updateBackground();
 
-        // As we have changed size, we should invalidate the outline so that is the the
-        // correct size
-        invalidateOutline();
     }
 
     @Override
@@ -157,4 +207,112 @@ public class FloatingActionButton extends FrameLayout implements Checkable {
         }
         return drawableState;
     }
+
+    private boolean hasLollipopApi() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
+    }
+
+    private boolean hasJellyBeanApi() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN;
+    }
+
+    private boolean hasHoneycombApi() {
+        return Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB;
+    }
+
+    private void updateBackground() {
+        StateListDrawable drawable = new StateListDrawable();
+        drawable.addState(new int[]{android.R.attr.state_checked}, createDrawable(mColorPressed));
+        drawable.addState(new int[]{}, createDrawable(mColorNormal));
+        setBackgroundCompat(drawable);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int size = getDimension(
+                mType == TYPE_NORMAL ? R.dimen.fab_size : R.dimen.fab_size_small);
+        if (mShadow && !hasLollipopApi()) {
+            size += mShadowSize * 2;
+            setMarginsWithoutShadow();
+        }
+        setMeasuredDimension(size, size);
+    }
+
+    private void setMarginsWithoutShadow() {
+        if (!mMarginsSet) {
+            if (getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
+                ViewGroup.MarginLayoutParams layoutParams = (ViewGroup.MarginLayoutParams) getLayoutParams();
+                int leftMargin = layoutParams.leftMargin - mShadowSize;
+                int topMargin = layoutParams.topMargin - mShadowSize;
+                int rightMargin = layoutParams.rightMargin - mShadowSize;
+                int bottomMargin = layoutParams.bottomMargin - mShadowSize;
+                layoutParams.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+
+                requestLayout();
+                mMarginsSet = true;
+            }
+        }
+    }
+
+    private Drawable createDrawable(int color) {
+        GradientDrawable shapeDrawable = new GradientDrawable();
+        shapeDrawable.setColor(color);
+        shapeDrawable.setShape(GradientDrawable.OVAL);
+        shapeDrawable.setSize(getWidth(), getHeight());
+        Log.d("width, height", getWidth() + " " + getHeight());
+
+        if (mShadow && !hasLollipopApi()) {
+            Drawable shadowDrawable = getResources().getDrawable(mType == TYPE_NORMAL ? R.drawable.shadow
+                    : R.drawable.shadow_mini);
+            LayerDrawable layerDrawable = new LayerDrawable(new Drawable[]{shadowDrawable, shapeDrawable});
+            layerDrawable.setLayerInset(1, mShadowSize, mShadowSize, mShadowSize, mShadowSize);
+            return layerDrawable;
+        } else {
+            return shapeDrawable;
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    @SuppressLint("NewApi")
+    private void setBackgroundCompat(Drawable drawable) {
+        if (hasLollipopApi()) {
+            float elevation;
+            if (mShadow) {
+                elevation = getElevation() > 0.0f ? getElevation()
+                        : getDimension(R.dimen.fab_elevation_lollipop);
+            } else {
+                elevation = 0.0f;
+            }
+            setElevation(elevation);
+            RippleDrawable rippleDrawable = new RippleDrawable(new ColorStateList(new int[][]{{}},
+                    new int[]{mColorRipple}), drawable, null);
+            setOutlineProvider(new ViewOutlineProvider() {
+                @Override
+                public void getOutline(View view, Outline outline) {
+                    outline.setOval(0, 0, getWidth(), getHeight());
+                }
+            });
+            setClipToOutline(true);
+            setBackground(rippleDrawable);
+        } else if (hasJellyBeanApi()) {
+            setBackground(drawable);
+        } else {
+            setBackgroundDrawable(drawable);
+        }
+    }
+
+    private int getDimension(@DimenRes int id) {
+        return getResources().getDimensionPixelSize(id);
+    }
+
+    private int getColor(@ColorRes int id) {
+        return getResources().getColor(id);
+    }
+
+    private TypedArray getTypedArray(Context context, AttributeSet attributeSet, int[] attr) {
+        return context.obtainStyledAttributes(attributeSet, attr, 0, 0);
+    }
+
+
 }
