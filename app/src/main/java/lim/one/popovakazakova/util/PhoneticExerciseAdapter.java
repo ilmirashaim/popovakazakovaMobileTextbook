9be500f35a9bdff5030cab187f0dea18eda41fb9
mira <ilmirashaim@gmail.com>
2015-04-09 11:38:45 +0500

    stop/play in phonetic exercises

--- app/src/main/java/lim/one/popovakazakova/util/PhoneticExerciseAdapter.java
+++ app/src/main/java/lim/one/popovakazakova/util/PhoneticExerciseAdapter.java
@@ -8,10 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -24,6 +24,7 @@ public class PhoneticExerciseAdapter extends BaseAdapter {
     private Context context;
     private LayoutInflater inflater;
     private MediaPlayer mp = new MediaPlayer();
+    private ViewGroup active = null;
 
     private String[] colors = new String[]{"red", "pink", "purple", "indigo", "green", "teal"};
 
@@ -59,7 +60,7 @@ public class PhoneticExerciseAdapter extends BaseAdapter {
         final TextView title = (TextView) convertView.findViewById(R.id.ex_title);
         title.setText(phoneticExercise.getTitle());
 
-        final FrameLayout playButton = (FrameLayout) convertView.findViewById(R.id.play_button);
+        final ViewGroup playButton = (ViewGroup) convertView.findViewById(R.id.play_button);
         playButton.setOnClickListener(new OnPlayButtonListener(playButton, phoneticExercise.getFilename()));
 
         return convertView;
@@ -67,7 +68,6 @@ public class PhoneticExerciseAdapter extends BaseAdapter {
 
 
     private int getColor(int position) {
-
         int color = context.getResources().getIdentifier(
                 colors[position % colors.length],
                 "color",
@@ -80,54 +80,76 @@ public class PhoneticExerciseAdapter extends BaseAdapter {
     public int getCount() {
         return exercises.size();
     }
-    private class OnPlayButtonListener implements View.OnClickListener {
 
+    private class OnPlayButtonListener implements View.OnClickListener {
         private String filename;
 
-        private OnPlayButtonListener(final FrameLayout playButton, String filename) {
+        private OnPlayButtonListener(final ViewGroup playButton, String filename) {
             mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
                 @Override
                 public void onCompletion(MediaPlayer mp) {
                     mp.reset();
-                    ((ImageView)playButton.findViewById(R.id.play_button_image)).setImageDrawable(
-                            context.getResources().getDrawable(R.drawable.ic_av_stop)
-                    );
+                    setPlayImage(playButton);
                 }
             });
             this.filename = filename;
         }
 
-
         @Override
         public void onClick(View v) {
-            try {
-
-                if (mp.isPlaying()) {
-                    mp.stop();
-                    mp.reset();
-                    ((ImageView)v.findViewById(R.id.play_button_image)).setImageDrawable(
-                            context.getResources().getDrawable(R.drawable.ic_av_play_arrow)
-                    );
-                } else {
-                    AssetFileDescriptor afd = context.getAssets().openFd(filename);
-                    mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-                    mp.prepare();
-                    mp.start();
-                    ((ImageView)v.findViewById(R.id.play_button_image)).setImageDrawable(
-                            context.getResources().getDrawable(R.drawable.ic_av_stop)
-                    );
-                }
-            } catch (Exception e) {
-                Log.e("media player exception", "on click", e);
-            }
+            play((ViewGroup) v, filename);
         }
     }
 
+    private void setStopImage(ViewGroup playButton) {
+        setImage(playButton, R.drawable.ic_av_stop);
+    }
 
-    public void stopPlaying() {
-        if (mp == null) {
-            return;
+    private void setPlayImage(ViewGroup playButton) {
+        setImage(playButton, R.drawable.ic_av_play_arrow);
+    }
+
+    private void setImage(ViewGroup playButton, int id) {
+        ((ImageView) playButton.findViewById(R.id.play_button_image)).setImageDrawable(
+                context.getResources().getDrawable(id)
+        );
+    }
+
+    synchronized private void play(ViewGroup playButton, String filename) {
+        try {
+            if (playButton.equals(active)) {
+                if (mp.isPlaying()) {
+                    mp.stop();
+                    mp.reset();
+                    setPlayImage(playButton);
+                } else {
+                    playFile(filename);
+                    setStopImage(playButton);
+                }
+            } else {
+                if (active != null) {
+                    setPlayImage(active);
+                }
+                stopPlaying();
+                mp.reset();
+                playFile(filename);
+                setStopImage(playButton);
+                active = playButton;
+            }
+        } catch (Exception e) {
+            Log.e("media player exception", "on click", e);
         }
+    }
+
+    synchronized private void playFile(String filename) throws IOException {
+        AssetFileDescriptor afd = context.getAssets().openFd(filename);
+        mp.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+        mp.prepare();
+        mp.start();
+    }
+
+
+    synchronized private void stopPlaying() {
         if (mp.isPlaying()) {
             mp.stop();
         }
